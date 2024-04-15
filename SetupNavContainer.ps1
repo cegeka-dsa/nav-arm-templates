ca856641b0f1d28289e18252fc464d3ed311c165
valsansilviu <167068367+valsansilviu@users.noreply.github.com>
2024-04-15 16:45:19 +0300

    update

--- SetupNavContainer.ps1
+++ SetupNavContainer.ps1
@@ -1,4 +1,3 @@
-
 ﻿if (!(Test-Path function:AddToStatus)) {
     function AddToStatus([string]$line, [string]$color = "Gray") {
         ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" -Force -ErrorAction SilentlyContinue
